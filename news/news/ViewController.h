e3ef868132fca976f974bc73397689a5bb420ce5
whtapple <453254593@qq.com>
2016-02-27 19:53:11 +0800

    新建项目

---
+++ news/news/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  news
+//
+//  Created by whtapple on 16/2/27.
+//  Copyright © 2016年 whtapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ news/news/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  news
+//
+//  Created by whtapple on 16/2/27.
+//  Copyright © 2016年 whtapple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
